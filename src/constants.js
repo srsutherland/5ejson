dfe474b6d60fcbf16a3f7590fbedefbc73251e78
Sean Sutherland <sean.sutherland@gotyto.com>
2023-06-21 14:44:58 -0700

    Add abbreviations to constants

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,12 @@
+const deepFreeze = (obj) => {
+    for (let key in obj) {
+        if (typeof obj[key] === 'object') {
+            deepFreeze(obj[key])
+        }
+    }
+    return Object.freeze(obj)
+}
+
 // eslint-disable-next-line no-unused-vars
 const abilityScoreNames = Object.freeze([
     "Strength",
@@ -46,3 +55,50 @@ const alignments = Object.freeze([
     'LN', 'N',  'CN', 
     'LE', 'NE', 'CE'
 ])
+
+// Abbreviations for long names
+// eslint-disable-next-line no-unused-vars
+const abbreviations = deepFreeze({
+    subclass: {
+        // First, words that are common to multiple most subclasses
+        "Circle of the ": "", // Druid
+        "Circle of ": "", // Druid
+        "College of ": "", // Bard
+        "Oath of ": "", // Paladin
+        "Way of the ": "", // Monk
+        "Way of ": "", // Monk
+        " Domain": "", // Cleric
+        "The ": "", // Warlock
+        "School of ": "", // Wizard
+        // Abbreviations for specific subclasses
+        // Barbarian
+        "Totem Warrior": "Totem",
+        "Storm Herald": "Storm",
+        "Ancestral Guardian": "Ancestral",
+        // Bard
+        // Cleric
+        // Druid
+        // Fighter
+        "Battle Master": "BM",
+        "Eldritch Knight": "EK",
+        // Monk
+        // Paladin
+        // Ranger
+        "Gloom Stalker": "Gloom",
+        "Horizon Walker": "Horizon",
+        // Rogue
+        // Sorcerer
+        "Abberrant Mind": "Abberrant",
+        "Clockwork Soul": "Clockwork",
+        "Divine Soul": "Divine",
+        "Draconic Bloodline": "Draconic",
+        "Shadow Magic": "Shadow",
+        "Storm Sorcery": "Storm",
+        "Wild Magic": "Wild",
+        // Warlock
+        "Great Old One": "GOO",
+        "Raven Queen": "RQ",
+        // Wizard
+        "Order of Scribes": "Scribes",
+    }
+});
