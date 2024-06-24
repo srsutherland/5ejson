35c4edbe00523e90e2162ab64bb3b8950a88cc15
Sean Sutherland <SeanRSuther@gmail.com>
2024-06-24 13:47:56 -0700

    Add source link

--- vue/components/sheet/SheetHeader.vue
+++ vue/components/sheet/SheetHeader.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { defineProps } from 'vue'
 
+import SourceLink from './SourceLink.vue'
+
 const props = defineProps({
     char: Object
 })
@@ -8,7 +10,10 @@ const props = defineProps({
 
 <template>
     <header id="top-header">
-        <h1 id="name">{{ props.char.name || "Name" }}</h1>
+        <h1 id="name">
+            {{ props.char.name || "Name" }}
+            <SourceLink :char="props.char"/>
+        </h1>
         <h2 id="race">{{ props.char.race }}</h2>
         <h2 id="class">{{ props.char.class }}</h2>
     </header>
---
+++ vue/components/sheet/SourceLink.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+    char: Object
+})
+</script>
+
+<template>
+    <div v-if="props.char.dndb_data?.id" class="source-link">
+        <a :href="'https://www.dndbeyond.com/characters/' + char.dndb_data.id">
+            <img src="https://www.google.com/s2/favicons?sz=16&amp;domain=www.dndbeyond.com">
+            🡥
+        </a>
+    </div>
+    <div v-if="props.char.roll20_data" class="source-link">
+        <img src="https://www.google.com/s2/favicons?sz=16&amp;domain=app.roll20.net">
+    </div>
+</template>
+
+<style scoped>
+.source-link {
+    margin-left: .5em;
+    font-size: 16px;
+    color: grey;
+}
+
+.source-link a {
+    color: grey;
+    text-decoration: none;
+}
+
+.source-link img {
+    max-width: 16px;
+    max-height: 16px;
+}
+</style>
